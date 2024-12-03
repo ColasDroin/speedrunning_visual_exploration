72052d80081e9c92102c4d149fa17f269a57a583
Colas Droin <colasdroin@gmail.com>
2024-12-03 19:27:35 +0100

    Trying to fix animation

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
