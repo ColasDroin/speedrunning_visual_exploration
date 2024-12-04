8c4d3825ac1587e0032d22298c951cf2e8f0e9b7
Colas Droin <colasdroin@gmail.com>
2024-12-04 14:51:27 +0100

    Progressing with banner

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -2,100 +2,31 @@ import styles from "../styles/banner.module.css";
 
 const Banner = () => {
   return (
-    <div
-      className={`${styles.background80s} ${styles.animatedClouds} ${styles.stars}`}
-    >
-      <div className={styles.sun}></div>
-      <div className={styles.grid}></div>
-      <div className={styles.mountain}></div>
+    <div className="h-full">
       <div
-        className={styles.mountain}
-        style={{
-          ["--mountain-base" as never]: "10vw",
-          ["--mountain-height" as never]: "5vw",
-          ["--mountain-color1" as never]: "#a684cb",
-          ["--mountain-color2" as never]: "#681e6b",
-          ["--mountain-tilt" as never]: "59deg",
-          ["--mountain-offset" as never]: "20vw",
-        }}
-      ></div>
-      {/* Repeat for other mountains */}
-      <div className={styles.roadOff}></div>
-      <div className={styles.overlay}></div>
-
-      <div className={styles.text}>
-        <div className={`${styles.outrun} ${styles.glow}`}>OUTRUN</div>
-        <div
-          className={`${styles.chrome} ${styles.shine}`}
-          data-text="80'S RETRO"
-        >
-          80<span className={styles.spark}></span>S RETRO
+        className={`${styles.background80s} ${styles.animatedclouds} ${styles.stars}`}
+      >
+        <div className={styles.coin}></div>
+        <div className={styles.starWrap}>
+          <div className={styles.star}></div>
         </div>
-        <div
-          className={`${styles.chrome} ${styles.shine}`}
-          data-text="DESIGN"
-          style={{ ["--shine-delay" as never]: "1s" }}
-        >
-          DESIGN
-          <span className={`${styles.spark} ${styles.sparkOffset}`}></span>
+        <div className={styles.grid}></div>
+        <div className={styles.progressLine}></div>
+
+        <div className={styles.overlay}></div>
+
+        <div className={styles.text}>
+          <div
+            className={`${styles.chrome} ${styles.shine}`}
+            data-text="SPEEDRUN"
+          >
+            SPEEDRUN<span className={styles.spark}></span>
+          </div>
+          <div className={`${styles.outrun} ${styles.glow}`}>
+            A DATA VIZ STORY
+          </div>
         </div>
       </div>
-
-      <svg width="0" height="0">
-        <filter id="filter">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency=".01"
-            numOctaves="10"
-            id="fractalNoise"
-          />
-          <feDisplacementMap
-            id="displacementMap"
-            in="SourceGraphic"
-            scale="120"
-          />
-        </filter>
-        <animate
-          xlinkHref="#displacementMap"
-          id="animclouds1"
-          begin="0; animclouds2.end"
-          attributeName="scale"
-          from="50"
-          to="180"
-          dur="3s"
-          fill="freeze"
-        />
-        <animate
-          xlinkHref="#displacementMap"
-          id="animclouds2"
-          begin="animclouds1.end"
-          attributeName="scale"
-          from="180"
-          to="50"
-          dur="3s"
-          fill="freeze"
-        />
-        <animate
-          xlinkHref="#fractalNoise"
-          id="animclouds3"
-          begin="0;animclouds4.end"
-          attributeName="baseFrequency"
-          from="0.03"
-          to="0.01"
-          dur="30s"
-          fill="freeze"
-        />
-        <animate
-          xlinkHref="#fractalNoise"
-          id="animclouds4"
-          begin="animclouds3.end"
-          attributeName="baseFrequency"
-          from="0.01"
-          to="0.03"
-          dur="30s"
-          fill="freeze"
-        />
-      </svg>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Banner from './components/banner';
+import Banner from "./components/banner";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Banner />
+    <div>
+      <div className="">
+        <Banner />
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {/* <Image
           className="dark:invert"
@@ -86,7 +88,7 @@ export default function Home() {
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
-          rel='noopener noreferrer'
+          rel="noopener noreferrer"
         >
           <Image
             aria-hidden
