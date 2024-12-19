c89c4d5146f26e4ed96f0c9d182181b0fbe8f32c
Colas Droin <colasdroin@gmail.com>
2024-12-19 09:04:44 +0100

    First version fig 4

--- app/components/fig4.tsx
+++ app/components/fig4.tsx
@@ -2,7 +2,47 @@
 // Import necessary libraries
 import React, { useRef, useEffect, useState } from "react";
 import ReactECharts from "echarts-for-react";
+import flagData from "../../public/data/flag_data.json";
 import pako from "pako";
+
+interface Flag {
+  name: string;
+  emoji: string;
+}
+function getFlag(countryCode: string) {
+  if (!countryCode) {
+    return "";
+  }
+  return (
+    flagData.find(function (item) {
+      return item.code === countryCode;
+    }) || {}
+  ).emoji;
+}
+
+const countryColors: Record<string, string> = {
+  Australia: "#00008b",
+  Canada: "#f00",
+  China: "#ffde00",
+  Cuba: "#002a8f",
+  Finland: "#003580",
+  France: "#ed2939",
+  Germany: "#000",
+  Iceland: "#003897",
+  India: "#f93",
+  Japan: "#bc002d",
+  "North Korea": "#024fa2",
+  "South Korea": "#000",
+  "New Zealand": "#00247d",
+  Norway: "#ef2b2d",
+  Poland: "#dc143c",
+  Russia: "#d52b1e",
+  Turkey: "#e30a17",
+  "United Kingdom": "#00247d",
+  "United States": "#b22234",
+};
+
+const updateFrequency = 2000;
 
 const Page: React.FC = () => {
   const chartRef = useRef<ReactECharts | null>(null);
@@ -11,133 +51,142 @@ const Page: React.FC = () => {
   // Function to prepare scatter options
   const prepareGraph = async () => {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
-    let popularityData: any[] = [];
+    let raceData: any[] = [];
 
     // Fetch and decompress scatter data
     try {
-      const response = await fetch(`${baseUrl}/data/popularity_data.json.gz`);
+      const response = await fetch(`${baseUrl}/data/race_data.json.gz`);
       const buffer = await response.arrayBuffer();
       const decompressed = pako.inflate(new Uint8Array(buffer), {
         to: "string",
       });
-      popularityData = JSON.parse(decompressed);
+      raceData = JSON.parse(decompressed);
     } catch (error) {
       console.error("Error fetching scatter data:", error);
       return;
     }
 
+    const headerData = raceData.header;
+    const data = raceData.data;
+
+    const yearMonth: string[] = [];
+    for (let i = 0; i < data.length; ++i) {
+      if (
+        yearMonth.length === 0 ||
+        yearMonth[yearMonth.length - 1] !== data[i][2]
+      ) {
+        yearMonth.push(data[i][2]);
+      }
+    }
+
+    const startIndex = 1;
+    const startMonth = yearMonth[startIndex];
+
     // Create the new chart option
-    const newOption: echarts.EChartsOption = {
-      title: {
-        text: "Evolution of game popularity with time",
+    const option: echarts.EChartsOption = {
+      grid: {
+        top: 10,
+        bottom: 30,
+        left: 150,
+        right: 80,
       },
-
-      animation: "auto",
-      animationDuration: 1000,
-      animationDurationUpdate: 500,
-      animationEasing: "cubicInOut",
-      animationEasingUpdate: "cubicInOut",
-      animationThreshold: 20000,
-      progressiveThreshold: 30000,
-      progressive: 400,
-      hoverLayerThreshold: 30000,
-      stateAnimation: {
-        duration: 300,
-        easing: "cubicOut",
-      },
-      tooltip: {
-        trigger: "axis",
-        axisPointer: {
-          type: "line",
-          lineStyle: {
-            color: "rgba(0,0,0,0.2)",
-            width: 1,
-            type: "solid",
-          },
-          label: {
-            show: true,
-            formatter: "{value|YYYY-MM}",
-          },
-        },
-        position: function (point, params, dom, rect, size) {
-          const chartWidth = size.viewSize[0];
-          const chartHeight = size.viewSize[1];
-          const tooltipHeight = size.contentSize[1];
-          let x =
-            point[0] < chartWidth / 3
-              ? chartWidth - size.contentSize[0] - 10
-              : 10;
-          let y = point[1] - 200;
-
-          // Ensure tooltip does not go below the lower limit of the graph
-          if (y + tooltipHeight > chartHeight) {
-            y = chartHeight - tooltipHeight - 10;
-          }
-
-          // Ensure tooltip does not go above the upper limit of the graph
-          if (y < 0) {
-            y = 10;
-          }
-
-          return [x, y];
-        },
-      },
-      legend: [
-        {
-          // selectedMode: 'single',
-          data: popularityData.games,
-        },
-      ],
-      singleAxis: {
-        top: 50,
-        bottom: 50,
-        axisTick: {
-          interval: 1, // Add ticks every month
-        },
+      xAxis: {
+        max: "dataMax",
         axisLabel: {
-          formatter: (value: string) =>
-            new Date(value).toISOString().slice(0, 7),
-        },
-        type: "time",
-        axisPointer: {
-          animation: true,
-          label: {
-            show: true,
-            formatter: (params: any) =>
-              new Date(params.value).toISOString().slice(0, 7),
+          formatter: function (n: number) {
+            return Math.round(n) + "";
           },
         },
-        splitLine: {
+      },
+      dataset: {
+        source: data.filter(function (d: string[]) {
+          return d[2] === startMonth;
+        }),
+      },
+      yAxis: {
+        type: "category",
+        inverse: true,
+        max: 10,
+        axisLabel: {
           show: true,
-          lineStyle: {
-            type: "dashed",
-            opacity: 0.5,
+          fontSize: 14,
+          formatter: function (value: any) {
+            return value + "{flag|" + getFlag(value) + "}";
+          },
+          rich: {
+            flag: {
+              fontSize: 25,
+              padding: 5,
+            },
           },
         },
+        animationDuration: 300,
+        animationDurationUpdate: 300,
       },
       series: [
         {
-          type: "themeRiver",
-          emphasis: {
-            focus: "series",
-            itemStyle: {
-              shadowBlur: 20,
-              shadowColor: "rgba(0, 0, 0, 0.8)",
+          realtimeSort: true,
+          seriesLayoutBy: "column",
+          type: "bar",
+          itemStyle: {
+            color: function (param) {
+              return countryColors[(param.value as number[])[0]] || "#5470c6";
             },
-            label: {
-              show: false, // Hide series labels
-            },
+          },
+          encode: {
+            x: 1,
+            y: 0,
           },
           label: {
-            show: false, // Hide series labels
+            show: true,
+            precision: 1,
+            position: "right",
+            valueAnimation: true,
+            fontFamily: "monospace",
           },
-          data: popularityData.data,
         },
       ],
+      // Disable init animation.
+      animationDuration: 0,
+      animationDurationUpdate: updateFrequency,
+      animationEasing: "linear",
+      animationEasingUpdate: "linear",
+      graphic: {
+        elements: [
+          {
+            type: "text",
+            right: 160,
+            bottom: 60,
+            style: {
+              text: startMonth,
+              font: "bolder 80px monospace",
+              fill: "rgba(100, 100, 100, 0.25)",
+            },
+            z: 100,
+          },
+        ],
+      },
     };
 
     // Update the chart option state
-    setOption(newOption);
+    setOption(option);
+
+    for (let i = startIndex; i < yearMonth.length - 1; ++i) {
+      (function (i) {
+        setTimeout(function () {
+          updateYear(yearMonth[i + 1]);
+        }, (i - startIndex) * updateFrequency);
+      })(i);
+    }
+
+    function updateYear(year: string) {
+      const source = data.filter(function (d: string[]) {
+        return d[2] === year;
+      });
+      (option as any).series[0].data = source;
+      (option as any).graphic.elements[0].style.text = year;
+      setOption(option);
+    }
   };
 
   // Use effect to prepare options and initialize the chart
