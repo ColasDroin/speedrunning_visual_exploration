b8856756bb1bfe2687b812b05a5c3902d641214b
Colas Droin <colasdroin@gmail.com>
2024-12-28 11:23:11 +0100

    Working banner but not greatly responsive

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -6,25 +6,30 @@ const Banner = () => {
       <div
         className={`${styles.background80s} ${styles.animatedclouds} ${styles.stars}`}
       >
-        <div className={styles.coin}></div>
-        <div className={styles.starWrap}>
-          <div className={styles.star}></div>
-        </div>
         <div className={styles.grid}></div>
         <div className={styles.progressLine}></div>
-
         <div className={styles.overlay}></div>
 
-        <div className={styles.text}>
-          <div
-            className={`${styles.chrome} ${styles.shine}`}
-            data-text="SPEEDRUNNING"
-          >
-            SPEEDRUNNING
+        {/* Hero Content Container */}
+        <div className={styles.heroContent}>
+          {/* Coin on the Left */}
+          <div className={styles.coin}></div>
+
+          {/* Text in the Center */}
+          <div className={styles.heroText}>
+            <div
+              className={`${styles.chrome} ${styles.shine}`}
+              data-text="SPEEDRUNNING"
+            >
+              SPEEDRUNNING
+            </div>
+            <div className={`${styles.outrun} ${styles.glow}`}>
+              How a community has pushed the limits of what was thought possible
+            </div>
           </div>
-          <div className={`${styles.outrun} ${styles.glow}`}>
-            How a community has pushed the limits of what was thought possible
-          </div>
+
+          {/* Star on the Right */}
+          <div className={styles.star}></div>
         </div>
       </div>
     </div>
